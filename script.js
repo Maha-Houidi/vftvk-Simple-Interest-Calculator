df84d13fbb820c87b5d4aa91e2126e1bc55ebaf0
Maha-Houidi <maha.houidi@etudiant-isi.utm.tn>
2021-07-14 14:25:40 +0000

    add validation in js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // calculating the interest when click on compute button 
 function compute()
 {
-    p = document.getElementById("principal").value;
+  
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
@@ -10,7 +10,7 @@ function compute()
     
   document.getElementById("result").innerHTML="If you deposit"
   +principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of " 
-  +amount+",\<br\>in the year "+year+"\<br\>" ;
+  +interest+",\<br\>in the year "+year+"\<br\>" ;
 }
 // reading the value of the rate in the range slider
 function updateRate() 
@@ -18,4 +18,15 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-        
+
+// 
+function validation(){
+    p = document.getElementById("principal").value;
+    if(p<=0){
+        alert("Enter a positive number");
+        return false;
+    }else{
+        return true;
+    }
+}
+  
