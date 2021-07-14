b85a639a1f2657b30650b0bf1076aac317d9a983
Maha-Houidi <maha.houidi@etudiant-isi.utm.tn>
2021-07-14 11:31:02 +0000

    modify js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+// calculating the interest when click on compute button 
 function compute()
 {
     p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
     
+  document.getElementById("result").innerHTML="If you deposit"
+  +principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of " 
+  +amount+",\<br\>in the year "+year+"\<br\>" 
+}
+// reading the value of the rate in the range slider
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
