dcce4f3c663aace38a4d20c73f4a0a2109f37edf
Maha-Houidi <maha.houidi@etudiant-isi.utm.tn>
2021-07-14 11:40:31 +0000

    modify js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function compute()
     
   document.getElementById("result").innerHTML="If you deposit"
   +principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of " 
-  +amount+",\<br\>in the year "+year+"\<br\>" 
+  +amount+",\<br\>in the year "+year+"\<br\>" ;
 }
 // reading the value of the rate in the range slider
 function updateRate() 
